18554c8901175dc783f502da21e9676b7891b163
Evgeniia Nikonova <beagle-elgaeb@ya.ru>
2021-05-31 11:04:32 +0300

    Добавление сохранения пропорций фото

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,27 @@
 const popups = document.querySelectorAll(".popup");
-const buttonsClosePopup = document.querySelectorAll(".popup__button-close");
+const closePopupButtons = document.querySelectorAll(".popup__button-close");
+
+// Попап редактирования профиля ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const editProfilePopup = document.querySelector(".popup_type_edit-profile");
 const editProfileForm = document.querySelector(".popup__form_type_edit-profile");
 const editProfileButton = document.querySelector(".profile__button-edit");
 const fullNameElement = document.querySelector(".profile__info-full-name");
-const fullNameInput = document.querySelector(".popup__form-item_text_full-name");
 const professionElement = document.querySelector(".profile__info-profession");
+const fullNameInput = document.querySelector(".popup__form-item_text_full-name");
 const professionInput = document.querySelector(".popup__form-item_text_profession");
+
+// Попап добавления карточки ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const addCardPopup = document.querySelector(".popup_type_add-card");
 const addCardForm = document.querySelector(".popup__form_type_add-card");
-const addCardbutton = document.querySelector(".profile__button-add-card");
+const addCardButton = document.querySelector(".profile__button-add-card");
 const picInput = document.querySelector(".popup__form-item_url_pic");
 const titleInput = document.querySelector(".popup__form-item_text_title");
+
+// Карточки ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const cardTemplate = document.querySelector("#card-template").content;
 const cardForClone = cardTemplate.querySelector(".card");
 const cards = document.querySelector(".photo-gallery__cards");
 const picPopup = document.querySelector(".popup_type_pic");
-let mouseDownTarget;
-let mouseUpTarget;
 
 // Изначальные карточки ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const primalCards = [
@@ -59,7 +63,7 @@ function openPopupEdit() {
 
 // Открываем popup новой карточки ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-addCardbutton.addEventListener("click", openPopupAddCard);
+addCardButton.addEventListener("click", openPopupAddCard);
 
 function openPopupAddCard() {
   addCardPopup.classList.add("popup_opened");
@@ -74,12 +78,15 @@ function closePopup(popup) {
 // ~~~~~~~~~~~~~~~ кнопкой ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 popups.forEach(function(popup, i) {
-  buttonsClosePopup[i].addEventListener("click", function() {
+  closePopupButtons[i].addEventListener("click", function() {
     closePopup(popup);
   });
 });
 
 // ~~~~~~~~~~~~~~~ по нажатию во вне ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let mouseDownTarget;
+let mouseUpTarget;
 
 document.addEventListener("mousedown", function(event) {
   mouseDownTarget = event.target;
@@ -114,24 +121,28 @@ editProfileForm.addEventListener("submit", function(event) {
 
 function addCard(pic, title) {
   const card = cardForClone.cloneNode(true);
-  const cardTitle = card.querySelector(".card__title");
-  const cardPhoto = card.querySelector(".card__photo");
+  const cardTitleElement = card.querySelector(".card__title");
+  const popupTitleElement = picPopup.querySelector(".popup__pic-title");
+  const cardPhotoElement = card.querySelector(".card__photo");
+  const popupPhotoElement = picPopup.querySelector(".popup__pic");
+  const likeCardButtons = card.querySelector(".card__button-like");
+  const deleteCardButtons = card.querySelector(".card__button-delete");
   
-  cardPhoto.src = pic;
-  cardTitle.textContent = title;
+  cardPhotoElement.src = pic;
+  cardTitleElement.textContent = title;
 
-  card.querySelector(".card__button-like").addEventListener("click", function (event) {
+  likeCardButtons.addEventListener("click", function (event) {
     event.target.classList.toggle("card__button-like_active");
   });
 
-  card.querySelector(".card__button-delete").addEventListener("click", function () {
+  deleteCardButtons.addEventListener("click", function () {
     card.remove();
   });
 
-  cardPhoto.addEventListener("click", function () {
+  cardPhotoElement.addEventListener("click", function () {
     picPopup.classList.add("popup_opened");
-    picPopup.querySelector(".popup__pic").src = cardPhoto.src;
-    picPopup.querySelector(".popup__pic-title").textContent = cardTitle.textContent;
+    popupPhotoElement.src = cardPhotoElement.src;
+    popupTitleElement.textContent = cardTitleElement.textContent;
   });
 
   cards.prepend(card);
